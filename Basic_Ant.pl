513926e7a2dac2f06a4ce48f795e42a7b9ba3b11
Keehl-Mihael <flaviusquatraro@gmail.com>
2019-12-05 14:08:46 +0100

    Movement Definition

--- Basic_Ant.pl
+++ Basic_Ant.pl
@@ -1,2 +1,33 @@
-leftMove(Cell(ant)) :- isFree(leftCell(Cell(ant))).
-rightMove(Cell(ant)) :- isFree(rightCell(Cell(ant))).
+interface ant
+predicates
+	canLeftMove:() ->(boolean Z).	
+	canRightMove:() ->(boolean Z).
+	canUpMove:() ->(boolean Z).
+	canDownMove:() ->(boolean Z).
+	getCell:()-> (integer X,integer Y).
+end interface ant
+
+class ant:ant
+constructors
+	new:(integer Id):-
+		Id=0.
+	/*new :(integer Id,integer X,integer Y):-
+		Id=random(),
+		Position=(randomX(),randomY()).*/
+end class
+
+implement ant
+facts
+	Id:integer.
+	Position:(integer X,integer Y).
+clauses
+	canLeftMove() :- isFree(leftCell(Cell(ant))).
+clauses
+	rightMove(Cell(ant)) :- isFree(rightCell(Cell(ant))).
+clauses
+	upMove(Cell(ant)) :- isFree(upCell(Cell(ant))).
+clauses
+	downMove(Cell(ant)) :- isFree(downCell(Cell(ant))).
+clauses
+	Cell(ant,cell):- cell is Position 
+
