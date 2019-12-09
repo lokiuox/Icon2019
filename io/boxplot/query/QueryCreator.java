4ce412084a8f2c5a1fbe3e28f8c33236252511b5
Arkango <saracinoarcangelo@gmail.com>
2019-12-09 10:54:19 +0100

    interfaccia query capace di gestire differenti risultati

--- io/boxplot/query/QueryCreator.java
+++ io/boxplot/query/QueryCreator.java
@@ -1,27 +1,66 @@
 package io.boxplot.query;
 
-public class QueryCreator {
+import org.jpl7.Query;
+import org.jpl7.Term;
 
-    int type;
+import java.util.Map;
+
+public class QueryCreator implements QueryDefiner{
+
+    int nSolutions = 0;
+    String predicate = "";
+    Query query;
+    Boolean resultBoolean;
+    private Object result;
+    Boolean is_valid = false;
+
+    public QueryCreator(){}
 
 
-    public QueryCreator(){
-
+    public void setPredicate(String predicate){
+        this.is_valid = false;
+        this.predicate = predicate;
     }
 
-    public QueryCreator(int type){
-        this.setType(type);
+    public Boolean getBoolean(){
+        if(is_valid && !predicate.isEmpty()){ return resultBoolean;}
+        query = new Query(predicate);
+        this.is_valid = true;
+        return resultBoolean = query.hasSolution();
     }
 
+    public Map<String, Term> getResult(){
+        if(is_valid && !predicate.isEmpty() && this.nSolutions == 1){ return (Map<String, Term>)  this.result;}
+        query = new Query(predicate);
+        this.is_valid = true;
 
-    private void setType(int type){
-        //check if type is known
-        if(type != 0 && type != 1){
-            System.out.println("type not recognized");
-        }else {
-            this.type = type;
+        this.nSolutions = query.allSolutions().length;
+
+        if(this.nSolutions > 1) {
+            System.out.println("Attention one solution returned, but there are others");
+            this.result = query.nextSolution();
+            return (Map<String, Term>) this.result;
+
         }
+        this.result = query.oneSolution();
+        return (Map<String, Term>) this.result;
     }
 
 
+    public Map<String, Term>[] getResults(){
+        if(is_valid && !predicate.isEmpty() && this.nSolutions > 1){ return (Map<String, Term>[])  this.result;}
+        query = new Query(predicate);
+        this.is_valid = true;
+
+        this.nSolutions = query.allSolutions().length;
+
+        if(this.nSolutions == 1) {
+            System.out.println("Attention only one solution available");
+            this.result = query.oneSolution();
+            return (Map<String, Term>[]) this.result;
+
+        }
+        this.result = query.allSolutions();
+        return (Map<String, Term>[]) this.result;
+    }
 }
